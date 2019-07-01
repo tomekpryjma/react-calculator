8b54276ee0387b592ced7818111b1aee171c6bdd
Tomek <tompe97@tlen.pl>
2019-07-01 20:59:40 +0100

    Add OPERATIONS const to config

--- src/components/calculator/operations/_config.js
+++ src/components/calculator/operations/_config.js
@@ -3,6 +3,15 @@
  * (34+3)+4/2-[2*(2*3)]*2+1-3
  */
 
+
+/**
+ * Need these as a constant as they are used in Buttonpad.js and will need
+ * to use them in equalise.js to sanitise input & make sure no duplicate
+ * operation symbols have been entered. Otherwise there is an infinite loop
+ * in carry-out-regex.js.
+ */
+const OPERATIONS = ["x", "/", "+", "-"];
+
 const BIDMAS = {
     brackets: {
         regex: /(\(\d*[\+\-\*\/]\d\))/gm
@@ -12,4 +21,7 @@ const BIDMAS = {
     }
 }
 
-export default BIDMAS;
+export {
+    BIDMAS,
+    OPERATIONS
+};
